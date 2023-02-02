980e54a1bcaf9b6fa42231c8eab4ab220e64727b
Halil DİKİŞLİ <halil851@gmail.com>
2023-02-02 08:49:03 +0300

    Add missing Interactor

--- MyContacts/addPerson-module/interactor/AddPersonInteractor.swift
+++ MyContacts/addPerson-module/interactor/AddPersonInteractor.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+let context = appDelegate.persistentContainer.viewContext
+
+class AddPersonInteractor: PresenterToInteractorAddPersonProtocol {
+    
+    func addPerson(_ contactName: String, _ contactPhoneNumber: String) {
+        let person = Contacts(context: context)
+        person.contactName = contactName.capitalized
+        person.contactPhoneNumber = contactPhoneNumber
+        
+        appDelegate.saveContext()
+    }
+    
+    
+}
